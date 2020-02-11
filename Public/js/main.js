926b7ea7908e7b7dc65a1767f17e221ee17b471b
nguyễn minh <minh072710@gmail.com>
2020-02-11 23:07:57 +0700

    first commit

--- Public/js/main.js
+++ Public/js/main.js
@@ -17,6 +17,55 @@ $(document).ready(function() {
 function out(){
     document.getElementById("load").removeAttribute("class");
     document.getElementById("load").innerHTML=`
-   
     `   
+}
+function expression(){
+    const name=document.getElementById('name').value;
+    const email=document.getElementById('email').value;
+    const phone=document.getElementById('phone').value;
+    const message=document.getElementById('message').value;
+    if (name == " " || name == "") {
+        document.getElementById('errname').innerHTML = " "
+
+
+    } else if (name.length < 5) {
+        document.getElementById('errname').innerHTML = "Ho và Tên phải từ 5 kí tự bất kì trở lên"
+
+    } else {
+        document.getElementById('errname').innerHTML = ""
+    }
+
+    if (phone == "") {
+        document.getElementById('errphone').innerHTML = ""
+    } else {
+        if (isNaN(phone)) {
+            document.getElementById('errphone').innerHTML = "Chỉ được nhập số"
+        } else if (isNaN(phone) || phone.length < 10 || phone.length > 11) {
+            document.getElementById('errphone').innerHTML = "Giá trị nằm trong khoảng 10-11 số!"
+        }
+        else {
+            document.getElementById('errphone').innerHTML = ""
+
+        }
+
+    }
+
+
+    if (email == "") {
+        document.getElementById('erremail').innerHTML = ""
+    } else {
+        if (email.indexOf('@') <= 0) {
+            document.getElementById('erremail').innerHTML = "Vị trí @ không hợp lệ!";
+        } else {
+            document.getElementById('erremail').innerHTML = "";
+
+        }
+        if (email.charAt(email.length - 4) != '.' && email.charAt(email.length - 3) != '.') {
+            document.getElementById('erremail').innerHTML = "Vị trí dấu . không hợp lệ!";
+
+        }
+    }
+    
+
+
 }
